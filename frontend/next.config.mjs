cad23ad6e67335ae54bf2cdfb6d32ac918186d14
Pedrzi <pedroh9@proton.me>
2026-02-15 03:52:15 +0000

    desfodendo a main parte 2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  }
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        // Tente usar '127.0.0.1' em vez de 'localhost' para evitar problemas de IPv6
+        destination: 'http://10.33.174.49:8000/api/:path*', 
+      },
+    ]
+  },
+};
 
-export default nextConfig
+export default nextConfig;
