90ba1936561eade935a86574a8ecc83d328599ed
Esteban-4413 <estebanyepezorozco@gmail.com>
2026-02-15 14:55:25 +0000

    wtv

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: '/api/:path*',
         // Tente usar '127.0.0.1' em vez de 'localhost' para evitar problemas de IPv6
-        destination: 'http://10.33.174.49:8000/api/:path*', 
+        destination: 'http://172.26.12.195:8000/api/:path*', 
       },
     ]
   },
